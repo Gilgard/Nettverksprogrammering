003b1cd5c44dd6e9a9b8550f5b0337ab0c4d889a
Alida <hjelljordalida@gmail.com>
2022-02-07 22:30:22 +0100

    todo i worker, snart ferdig

--- Øving2/Workers.java
+++ Øving2/Workers.java
@@ -1,33 +1,111 @@
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Workers{
-    private int numberOfThreads;
-    private Thread[] threads;
-    private Condition lock;
+    private Lock jobQueueLock = new ReentrantLock();
+    private Queue<Runnable> jobQueue = new ConcurrentLinkedQueue<>();
+    private Condition jobAvailable = jobQueueLock.newCondition();
+
+    private Lock workerListLock = new ReentrantLock();
+    private Queue<Thread> workers;
+    private Condition availableWorker = workerListLock.newCondition();
+
+    private boolean isRunning = false;
 
     public Workers(int numberOfThreads) {
-        this.numberOfThreads = numberOfThreads;
-        this.threads = new Thread[numberOfThreads];
+        this.workers = new ArrayBlockingQueue<>(numberOfThreads);
     }
 
     public void start() {
-        for(int i = 0; i < numberOfThreads; i++) {
-            threads[i] = new Thread();
-            threads[i].start();
-        }
+        isRunning = true;
+        work().start();
+    }
+
+    private Thread work() {
+        return new Thread(() -> {
+
+            while(isRunning) {
+                jobQueueLock.lock();
+
+                //====================================================
+                //Todo: gjør om runnable til jobb
+                //====================================================
+                
+                while (jobQueue.isEmpty() || jobQueue.peek().startTime > System.currentTimeMillis()) {
+                    try {
+                        if(!jobQueue.isEmpty()) {
+                            long waitTime = jobQueue.peek().startTime - System.currentTimeMillis();
+                            // Waiting for job-start time
+                            jobAvailable.await(waitTime, TimeUnit.MILLISECONDS);
+                        } else {
+                            // Waiting for new jobs to arrive in availableJobsList
+                            jobAvailable.await();
+                        }
+
+                        if(!isRunning) {
+                            //Consumer-thread will now exit to respect join()
+                            return;
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                // Adding new job to active jobs-list.
+                workerListLock.lock();
+
+                Thread jobThread = new Thread(jobQueue.poll());
+                while(!workers.offer(jobThread)) {
+                    try {
+                        availableWorker.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
     public void post(Runnable runnable) {
-        while()
+        jobQueueLock.lock();
+        jobQueue.add(runnable);
+        jobAvailable.signalAll();
+        jobQueueLock.unlock();
     }
 
     public void join() {
-        for(Thread i : threads){
+        joinAvailableJobs();
+        joinAvailableWorkers();
+    }
+
+    private void joinAvailableJobs() {
+        jobQueueLock.lock();
+        while (!jobQueue.isEmpty()) {
             try {
-                i.join();
+                jobAvailable.await();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        isRunning = false;
+        jobAvailable.signalAll();
+        jobQueueLock.unlock();
+    }
+    
+    public void joinAvailableWorkers() {
+        workerListLock.lock();
+        while(!workers.isEmpty()) {
+            try {
+                availableWorker.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        availableWorker.signalAll();
+        workerListLock.unlock();
     }
 }
