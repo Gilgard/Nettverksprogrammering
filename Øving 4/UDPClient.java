24df81b723094050bdd05e1cb3e073c6fd7bef4b
Alida <hjelljordalida@gmail.com>
2022-02-21 11:00:27 +0100

    FML

---
+++ Øving 4/UDPClient.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+public class UDPClient {
+    DatagramSocket ds = new DatagramSocket();
+    InetAddress inetAddress = InetAddress.getLocalHost();
+    byte[] byteArr = new byte[1024];
+    byte[] byteSend;
+    DatagramPacket dpReceive = new DatagramPacket(byteArr, byteArr.length);
+    DatagramPacket dpSend;
+
+    public UDPClient() throws SocketException, UnknownHostException {
+    }
+
+    public void start() throws IOException {
+        Scanner scNr = new Scanner(System.in);
+        int operation = -1;
+        while (operation != 0) {
+            System.out.println("________CALCULATOR________\n1: Subtraction\n2: Addition\n" +
+                    "3: Multiply\n4: Divide\n0: quit");
+            operation = scNr.nextInt();
+            if (operation == 0){
+                byteSend = "".getBytes();
+                dpSend = new DatagramPacket(byteSend, byteSend.length, inetAddress, 1001);
+                ds.send(dpSend);
+            }
+            if (!(operation < 0 || operation > 4)){
+                System.out.println("Enter first number");
+                int first = scNr.nextInt();
+                System.out.println("Enter number to equate");
+                int last = scNr.nextInt();
+
+                if (operation == 1) {
+                    byteSend = (first + "-" + last).getBytes();
+                } else if (operation == 2) {
+                    byteSend = (first + "+" + last).getBytes();
+                } else if (operation == 3) {
+                    byteSend = (first + "*" + last).getBytes();
+                } else {
+                    byteSend = (first + "/" + last).getBytes();
+                }
+                dpSend = new DatagramPacket(byteSend, byteSend.length, inetAddress, 1001);
+                ds.send(dpSend);
+
+                ds.receive(dpReceive);
+                System.out.println("The Result is " + (new String(dpReceive.getData(), 0, dpReceive.getLength())));
+            }
+        }
+    }
+
+    public void close() {
+        ds.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        UDPClient client = new UDPClient();
+        client.start();
+        client.close();
+    }
+}
