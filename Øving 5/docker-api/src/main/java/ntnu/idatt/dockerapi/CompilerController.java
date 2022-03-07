70adf7d370da42dcbb8ccfd4237cd069b485f9a1
mmedvard <mmedvard@stud.ntnu.no>
2022-03-07 15:08:38 +0100

    readProcess method

--- Øving 5/docker-api/src/main/java/ntnu/idatt/dockerapi/CompilerController.java
+++ Øving 5/docker-api/src/main/java/ntnu/idatt/dockerapi/CompilerController.java
@@ -1,9 +1,15 @@
 package ntnu.idatt.dockerapi;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collector;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerCmd;
@@ -58,8 +64,14 @@ public class CompilerController {
         return result;
     }
 
-    private String readProcess(InputStream stream) {
+    private String readProcess(InputStream stream) throws Error {
         //build buffered stream to string
-        return null;
+
+
+        BufferedInputStream buffy = new BufferedInputStream(stream);
+
+        String string = new StringBuilder(buffy.read());
+
+        return string;
     }
 }
