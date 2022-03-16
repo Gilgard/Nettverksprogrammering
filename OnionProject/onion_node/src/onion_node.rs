0e112ff388010d159fc98d1f16fd9ade9e10e885
Alida <hjelljordalida@gmail.com>
2022-03-16 20:49:03 +0100

    rip

--- OnionProject/onion_node/src/onion_node.rs
+++
@@ -1,6 +0,0 @@
-// needs to be able to decrypt packages and pass it on:
-
-
-// if its the end node recieving data from a third party it needs to be able to encrypt with layers:
-// get request sent to third parties should probably wait for set up a thread that waits for reply, 
-// finds a path back to source and encrypts.
--- OnionProject/onion_router/src/onion_router.rs
+++
@@ -1,2 +0,0 @@
-// should be able to find a path and encrypt layers
-// needs access to a key list of some sort to be able to commuicate with nodes 
